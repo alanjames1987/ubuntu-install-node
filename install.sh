984a40a94b618fe7eb35c4d42d05bc3b637f1605
Alan James <alanjames1987@gmail.com>
2014-01-18 02:59:30 -0500

    Commit

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get install -y g
 
 sudo apt-get install -y mongodb
 
-sudo apt-get install -y g++
+sudo apt-get install -y g++ iptables-persistent
 
 git clone https://github.com/creationix/nvm.git ~/.nvm
 source ~/.nvm/nvm.sh
@@ -14,8 +14,8 @@ nvm install 0.10.21
 npm install forever -g
 npm install node-dev -g
 
-echo 'export PATH=$PATH:/home/adminuser/.nvm/v0.10.21/bin/' >> ~/.bashrc
-echo 'alias sudo="sudo env PATH=$PATH"' >> ~/.bashrc
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+sudo iptables-save > /etc/iptables/rules.v4
 
 sudo apt-get -y autoclean && sudo apt-get -y autoremove
 
