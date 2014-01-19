e94e5477bcde19c6c37e52a253a1385ba7afbc09
Alan James <alanjames1987@gmail.com>
2014-01-18 21:16:28 -0500

    Commit

--- install-nosu.sh
+++ install-nosu.sh
@@ -8,6 +8,8 @@ echo 'source ~/.nvm/nvm.sh' >> ~/.profile
 # install node
 nvm install 0.10.21
 
+nvm alias default 0.10.21
+
 # install other useful tools
 npm install forever -g
 npm install node-dev -g
